34b4b3856c5a01ba631f56baaea81ca4dd85436c
Walter Kampitsch <wkampitsch@me.com>
2016-12-24 20:43:08 -0500

    fix indentations to spaces

--- make_mask.py
+++ make_mask.py
@@ -1,51 +1,51 @@
 # coding: utf-8
 import numpy as np
 
-def	make_mask(array, labels=('yes', 'no'), rule='far'):
-	stack = []
-	result = []
-	flag = False
-	for n, item in enumerate(array):
-		if item == labels[0]:
-			stack.append(n)
-			flag = True
-		if flag and item == labels[1]:
-			if rule == 'close':
-				result.append(stack[-1])
-				result.append(n)
-				stack = []
-				flag = False
-			elif rule == 'far':
-				result.append(stack[0])
-				result.append(n)
-				stack = []
-				flag = False
-			else:
-				raise ValueError("Not supported rule ", rule)
-	mask = np.zeros(len(array), dtype=int)
-	mask[result] = 1
-	return mask
-	
+def make_mask(array, labels=('yes', 'no'), rule='far'):
+  stack = []
+  result = []
+  flag = False
+  for n, item in enumerate(array):
+    if item == labels[0]:
+      stack.append(n)
+      flag = True
+    if flag and item == labels[1]:
+      if rule == 'close':
+        result.append(stack[-1])
+        result.append(n)
+        stack = []
+        flag = False
+      elif rule == 'far':
+        result.append(stack[0])
+        result.append(n)
+        stack = []
+        flag = False
+      else:
+        raise ValueError("Not supported rule ", rule)
+  mask = np.zeros(len(array), dtype=int)
+  mask[result] = 1
+  return mask
+
 def print_mask(array, mask):
-	print('{:>5} - {:^4} - {:<6}'.format('label', 'mask', 'result'))
-	print('-'*21)
-	rows = []
-	for i, z in enumerate(zip(array, mask)):
-		c, m = z
-		if mask[i]:
-			t = array[i]
-		else:
-			t = ''	
-		rows.append('{:>5} - {:^4} - {:<6}'.format(c, m, t))
-	print('\n'.join(rows))
-	print('\n')
+  print('{:>5} - {:^4} - {:<6}'.format('label', 'mask', 'result'))
+  print('-'*21)
+  rows = []
+  for i, z in enumerate(zip(array, mask)):
+    c, m = z
+    if mask[i]:
+      t = array[i]
+    else:
+      t = ''
+    rows.append('{:>5} - {:^4} - {:<6}'.format(c, m, t))
+  print('\n'.join(rows))
+  print('\n')
 
 def run_tbl(array, labels, rules):
-	for label in labels:
-		for rule in rules:
-			print("Label:{} Rule:{}\n".format(label, rule))
-			mask = make_mask(array, labels=label, rule=rule)
-			print_mask(array, mask)
+  for label in labels:
+    for rule in rules:
+      print("Label:{} Rule:{}\n".format(label, rule))
+      mask = make_mask(array, labels=label, rule=rule)
+      print_mask(array, mask)
 
 y_n = np.array(['no', 'no', 'yes', 'yes', 'no', 'no', 'yes', 'no', 'no'])
 on_off = np.array(['on', 'on', 'off', 'on', 'off', 'off', 'off', 'on', 'on'])
